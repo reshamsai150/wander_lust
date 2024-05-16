a17127b35010adb200e0346d17c3ac2720905cde
Saipranathi <reshamsai150@gmail.com>
2024-05-16 15:20:52 +0530

    done

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-
-
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main()
     .then(()=>{
@@ -21,7 +21,9 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 app.get("/",(req,res)=>{
 
     res.send("hello iam root");
@@ -71,12 +73,27 @@ app.post("/listings", async (req, res) => {
       const newListing = new Listing(req.body.listing);
       await newListing.save();
       res.redirect("/listings");
-  // } catch (error) {
-  //     console.error("Error creating new listing:", error); // Log the specific error
-  //     res.status(500).send("Internal Server Error");
-  // }
-});
 
+});
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+  let { id } = req.params;
+    const listing = await Listing.findById(id);
+res.render("listings/edit.ejs",{listing})
+  });
+//update route
+app.put("/listings/:id",async(req,res)=>{
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings/${id}`);
+});
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+  let { id } = req.params;
+  const deletedListing= await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing=new Listing({
 //         title:"my new villa",
