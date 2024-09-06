6fce421dfc7b3277647988ac5f502635c35a4a78
Resham Saipranathi <164533685+reshamsai150@users.noreply.github.com>
2024-09-06 21:19:25 +0530

    done

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ res.redirect(`/listings/${listing._id}`)
 app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
   
   let {id,reviewId}=req.params;
-  await Listing.findByIdAndUpdate(id,{$pull:{review:reviewId}});
+  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
   await Review.findByIdAndDelete(reviewId);
   res.redirect(`/listings/${id}`);
 }))
