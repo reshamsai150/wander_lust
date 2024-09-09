bfff27ada9815bc824606f5fc4938c9267bb4014
Resham Saipranathi <164533685+reshamsai150@users.noreply.github.com>
2024-09-09 20:24:50 +0530

    done

--- classroom/server.js
+++ classroom/server.js
@@ -16,16 +16,25 @@ const sessionOptions=({secret:"mysupersecretstring",
 app.use(session(sessionOptions));
 app.use(flash());
 
-
+app.use((req,res,next)=>{
+    res.locals.successMsg=req.flash("success");
+    res.locals.errorMsg=req.flash("error");
+    next();
+});
 app.get("/register",(req,res)=>{
     let {name="anonymous"}=req.query;
    req.session.name=name;
-   req.flash("succes","user registered successully")
-    res.redirect("/hello");
+   
+   if(name==="anonymous"){
+    req.flash("error","user not registered");
+   }else{ req.flash("success","user registered successully!");
+   }
+  
+   res.redirect("/hello");
 });
 app.get("/hello",(req,res)=>{
-    
-    res.render("page.ejs",{name:req.session.name,msg:req.flash("success")});
+res.locals.messages=req.flash("success");
+    res.render("page.ejs",{name:req.session.name});
 });
 
 app.get("/test",(req,res)=>{
