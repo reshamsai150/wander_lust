9afa0642fa0f97f9bd52f02311875402a3142311
Resham Saipranathi <164533685+reshamsai150@users.noreply.github.com>
2024-09-20 22:55:06 +0530

    done

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express=require("express");
 const router=express.Router();
-const User=required("../models/user.js");
+const User=require("../models/user.js");
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
 
-router.post("/signup",(req,res)=>{
+router.post("/signup",async(req,res)=>{
     let {username,email,password}=req.body;
     const newUser=new User({email,username});
     const registeredUser= await User.register(newUser,password);
