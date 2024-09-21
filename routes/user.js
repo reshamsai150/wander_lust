a06fb06c9253e14a908800a17fe37fb7ba98e0f8
Resham Saipranathi <164533685+reshamsai150@users.noreply.github.com>
2024-09-21 07:55:46 +0530

    done

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,27 @@
 const express=require("express");
 const router=express.Router();
 const User=require("../models/user.js");
+const wrapAsync=require("../utils/wrapAsync");
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
 
-router.post("/signup",async(req,res)=>{
+router.post("/signup",wrapAsync(async(req,res)=>{
+  try{
     let {username,email,password}=req.body;
     const newUser=new User({email,username});
     const registeredUser= await User.register(newUser,password);
     console.log(registeredUser);
     req.flash("success","welcome to wanderlust!");
     res.redirect("/listings");
+  }catch(e){
+ req.flash("error",e.message);
+ res.redirect("/signup");
+  }
+    
+   
+}));
+router.get("/login",(req,res)=>{
+    res.render("user/login.ejs");
 })
-
 module.exports=router;
