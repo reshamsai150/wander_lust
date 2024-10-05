62c7e8f4837c53989b83bde16cf3ed8d6cb0bf22
Resham Saipranathi <164533685+reshamsai150@users.noreply.github.com>
2024-10-05 22:26:16 +0530

    done

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
   res.locals.error=req.flash("error");
-  res.locals.curUser=req.user;
+  res.locals.currUser=req.user;
  next();
 });
 // app.get("/demouser",async(req,res)=>{
--- routes/listing.js
+++ routes/listing.js
@@ -24,7 +24,9 @@ router.get("/:id", async (req, res) => {
   try {
     let { id } = req.params;
    
-    const listing = await Listing.findById(id).populate("reviews").populate("owner");
+    const listing = await Listing.findById(id).populate({path:"reviews",populate: {
+      path:"author",
+    }}).populate("owner");
 
     if (!listing) {
       req.flash("error","Listing you requested for doesnot exist");
