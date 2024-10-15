0c7df3f5cef267a67eb9e688367017a51f885fe7
Resham Saipranathi <164533685+reshamsai150@users.noreply.github.com>
2024-10-15 21:30:17 +0530

    done

--- public/js/map.js
+++ public/js/map.js
@@ -1,20 +1,25 @@
+if (coordinates.length == 0) {
+  //if the coordinates is undefined = Default value(New Delhi)
+  coordinates = [77.1025, 28.7041];
+}
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v12",
-    center: coordinates,
-    zoom: 9
+  container: "map", // container ID
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
 });
 
-map.on('load', () => {
-    if (coordinates && coordinates.length === 2) {
-        const marker = new mapboxgl.Marker({ color: "red" })
-            .setLngLat(coordinates)
-            .setPopup(new mapboxgl.Popup({ offset: 25 })
-                .setHTML("<h2>Hello World!</h2>")
-            )
-            .addTo(map);
-    } else {
-        console.error("Invalid coordinates for marker:", coordinates);
-    }
-});
+console.log(coordinate);
+
+const marker1 = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.name}</h4>
+      <p>Exact Location will be provided after booking</p>`
+    )
+  )
+  .addTo(map);
+  
+
+// .addTo(map);
